0d495334f9ef1b5964fe1fe1b07563a6d5515338
Lucas Dachman <lucas.dachman@gmail.com>
2019-02-23 16:15:32 -0700

    remove sidebar from index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './components/Home';
-import Sidebar from './components/Sidebar';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('home'));
-ReactDOM.render(<Sidebar />, document.getElementById('sidebar'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
