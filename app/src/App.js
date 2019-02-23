f9c7d13ed016e8a68a79a7854645ccc08c3f6203
Costa, Stefano <stefano.costa@gmail.com>
2019-02-23 13:19:57 -0700

    First page without CSS

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
+import React,
+{
+  Component
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className = "App" >
+      <body>
+      <h3>Weight</h3>
+
+      <form>
+      <label>
+      <input type="text" name="name" />
+      </label>
+          <input type="submit" value="Submit" />
+      </form>
+
+
+
+      </body>
       </div>
     );
   }
