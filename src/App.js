6ea37ba3ad3f72db2180e8ca78c0c0278777db40
Lucas Dachman <lucas.dachman@gmail.com>
2019-02-23 14:41:31 -0700

    add input fields

--- src/App.js
+++ src/App.js
@@ -2,27 +2,53 @@ import React,
 {
   Component
 } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.goToMainPage = this.goToMainPage.bind(this)
+    this.setLbText = this.setLbText.bind(this)
+    this.setKgText = this.setKgText.bind(this)
+
+    this.state = {
+      lb: "",
+      kg: ""
+    }
+  }
+
+  goToMainPage(e) {
+    e.preventDefault()
+  }
+
+  setLbText(e) {
+    this.setState({lb: e.target.value})
+  }
+
+  setKgText(e) {
+    this.setState({kg: e.target.value})
+  }
+
   render() {
     return (
-      <div className = "App" >
-      <body>
-      <h3>Weight</h3>
-
-      <form>
-      <label>
-      <input type="text" name="name" />
-      </label>
-          <input type="submit" value="Submit" />
+      <form className="landing-form" onSubmit={this.goToMainPage}>
+        <label htmlFor="weight">Weight:</label>
+        {/* kg weight */}
+        <div>
+          <input type="number" 
+            value={this.state.lb}
+            onChange={this.setLbText}
+            name="weight" id='lb-weight' />
+        </div>
+        <div>
+        {/* kg weight */}
+        </div>
+          <input type="number" 
+            value={this.state.kg}
+            onChange={this.setKgText}
+            name="weight" id='kg-weight' />
+        <input type="submit" value="Calculate" />
       </form>
-
-
-
-      </body>
-      </div>
     );
   }
 }
