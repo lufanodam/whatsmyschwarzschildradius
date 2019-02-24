d1a025a706dc6fd4d7d8af733a6e981f45060461
Costa, Stefano <stefano.costa@gmail.com>
2019-02-23 20:58:29 -0700

    Updates

---
+++ precache-manifest.b3803b220ecd64e646949c17ddb52cc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9c746985f90d352f7a743ae0f65e724",
+    "url": "/lufanodam/static/media/Background.d9c74698.jpg"
+  },
+  {
+    "revision": "5bf3a726419d181b68c1",
+    "url": "/lufanodam/static/js/runtime~main.5bf3a726.js"
+  },
+  {
+    "revision": "2fa9da1ab4525278afbc",
+    "url": "/lufanodam/static/js/main.2fa9da1a.chunk.js"
+  },
+  {
+    "revision": "316f1e222dcf2ab1d9ac",
+    "url": "/lufanodam/static/js/2.316f1e22.chunk.js"
+  },
+  {
+    "revision": "2fa9da1ab4525278afbc",
+    "url": "/lufanodam/static/css/main.d0cb22af.chunk.css"
+  },
+  {
+    "revision": "eb8e4fd900e60474b3789c1bb6a7e50b",
+    "url": "/lufanodam/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mywebsite/precache-manifest.60a933427c128cc564e04d95e45b8801.js"
+  "/lufanodam/precache-manifest.b3803b220ecd64e646949c17ddb52cc0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/mywebsite/index.html", {
+workbox.routing.registerNavigationRoute("/lufanodam/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
