7084b583543b8ba9317efc39ec97312196e3c76c
Costa, Stefano <stefano.costa@gmail.com>
2019-02-23 20:49:19 -0700

    Updates

---
+++ precache-manifest.cadc664b9f8f5e7b13103bacc27eab95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9c746985f90d352f7a743ae0f65e724",
+    "url": "/lufanodam/whatsmyschwarzschildradius/static/media/Background.d9c74698.jpg"
+  },
+  {
+    "revision": "e8eeeec05aa41488aa11",
+    "url": "/lufanodam/whatsmyschwarzschildradius/static/js/runtime~main.e8eeeec0.js"
+  },
+  {
+    "revision": "9965c3e73a307127ea0a",
+    "url": "/lufanodam/whatsmyschwarzschildradius/static/js/main.9965c3e7.chunk.js"
+  },
+  {
+    "revision": "316f1e222dcf2ab1d9ac",
+    "url": "/lufanodam/whatsmyschwarzschildradius/static/js/2.316f1e22.chunk.js"
+  },
+  {
+    "revision": "9965c3e73a307127ea0a",
+    "url": "/lufanodam/whatsmyschwarzschildradius/static/css/main.9fc5223c.chunk.css"
+  },
+  {
+    "revision": "c654a601f54c0329e8e1e626fc57c374",
+    "url": "/lufanodam/whatsmyschwarzschildradius/index.html"
+  }
+];
