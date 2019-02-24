ef7c93f6bd7692da27e23bec7f5b6e6bc9deb8d8
Costa, Stefano <stefano.costa@gmail.com>
2019-02-23 20:55:10 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/lufanodam/whatsmyschwarzschildradius/precache-manifest.cadc664b9f8f5e7b13103bacc27eab95.js"
+  "/mywebsite/precache-manifest.60a933427c128cc564e04d95e45b8801.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/lufanodam/whatsmyschwarzschildradius/index.html", {
+workbox.routing.registerNavigationRoute("/mywebsite/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
